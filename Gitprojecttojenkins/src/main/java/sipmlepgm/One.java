50f61faae2a1bba5d421608d7d02bf533f90b804
Sony11M <sonymadathil11@gmail.com>
2023-08-07 21:15:12 +0530

    First Commit

---
+++ Gitprojecttojenkins/src/main/java/sipmlepgm/One.java
@@ -0,0 +1,23 @@
+package sipmlepgm;
+
+
+
+public class One
+{
+
+	public One()     //Default constructor creation
+	{
+		System.out.println();
+		int age= 10;
+	    String name= "Sony";
+	    System.out.println(age);
+	    System.out.println(name);
+	}
+
+	public static void main(String[] args) 
+	{
+		One con= new One();   
+
+	}
+
+}
